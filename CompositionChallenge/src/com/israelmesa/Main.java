aaf2aef0ff439bd5d195125e6b90d9d84455730f
Israel Mesa <mesa.israel50@gmail.com>
2018-11-07 13:57:52 -0500

    Update

--- CompositionChallenge/src/com/israelmesa/Main.java
+++ CompositionChallenge/src/com/israelmesa/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        Dimensions dishWasherDimensions = new Dimensions(24, 24, 24);
+        DishWasher theDishWasher = new DishWasher("Samsung", "Silver", dishWasherDimensions);
+
+        Dimensions ovenDimensions = new Dimensions(30, 30 , 30 );
+        Oven theOven = new Oven("Whirlpool", 4, "Black", ovenDimensions);
+
+        Sink theSink = new Sink("Kohler", "Stainless Steel", 2);
+
+        Kitchen theKitchen = new Kitchen(theDishWasher, theOven, theSink);
+
+        theKitchen.useDishWasher();
 
     }
 }
