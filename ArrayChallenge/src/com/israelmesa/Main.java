2225a60ee248d43674afbd3ff88bb1271f2285d1
Israel Mesa <mesa.israel50@gmail.com>
2018-11-28 19:44:49 -0500

    Alternative solution commented out

--- ArrayChallenge/src/com/israelmesa/Main.java
+++ ArrayChallenge/src/com/israelmesa/Main.java
@@ -1,5 +1,6 @@
 package com.israelmesa;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -59,5 +60,32 @@ public class Main {
         }
         return array;
     }
+
+//    public static int[] sortArray(int[] array) {
+//        int[] sortedArray = new int[array.length];
+//        for(int i=0; i<array.length; i++) {
+//            sortedArray[i] = array[i];
+//        }
+//
+//        int[] sortedArray = Arrays.copyOf(array, array.length);
+//
+//        boolean flag = true;
+//        int temp;
+//        while(flag) {
+//            flag = false;
+//
+//            for(int i=0; i<sortedArray.length-1; i++) {
+//                if(sortedArray[i] < sortedArray[i+1]) {
+//                    temp = sortedArray[i];
+//                    sortedArray[i] = sortedArray[i+1];
+//                    sortedArray[i+1] = temp;
+//                    flag = true;
+//                }
+//            }
+//        }
+//
+//        return sortedArray;
+//    }
+
 
 }
