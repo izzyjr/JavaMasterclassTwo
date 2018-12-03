d364e36f67a9bb33c322567f959b6003aab2dd5b
Israel Mesa <mesa.israel50@gmail.com>
2018-12-03 15:51:37 -0500

    Final Updates

--- ArrayListChallenge/src/com/israelmesa/Main.java
+++ ArrayListChallenge/src/com/israelmesa/Main.java
@@ -1,13 +1,94 @@
 package com.israelmesa;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone();
 
     public static void main(String[] args) {
 
-        MobilePhone mobilePhone = new MobilePhone();
-        mobilePhone.storeContact("Juan Valdez", "7221427");
-        mobilePhone.storeContact("Pedro Sanchez", "7221427");
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+        while (!quit) {
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
 
-
+            switch (choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    mobilePhone.printMobileContactList();
+                    break;
+                case 2:
+                    addPhoneContact();
+                    break;
+                case 3:
+                    modifyPhoneContact();
+                    break;
+                case 4:
+                    removePhoneContact();
+                    break;
+                case 5:
+                    searchPhoneCntact();
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+            }
+        }
     }
+
+    public static void printInstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of contacts.");
+        System.out.println("\t 2 - To add a contact to list.");
+        System.out.println("\t 3 - To modify a contact in the list.");
+        System.out.println("\t 4 - To remove a contact from the list.");
+        System.out.println("\t 5 - To search for a contact in the list.");
+        System.out.println("\t 6 - To quit the application.");
+    }
+
+    public static void addPhoneContact() {
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter phone number: ");
+        String phoneNumber = scanner.nextLine();
+        mobilePhone.storeContact(name, phoneNumber);
+        System.out.println("Contact " + mobilePhone.getNameContact(name) + " has been saved successfully.");
+    }
+
+    public static void modifyPhoneContact() {
+        System.out.println("Enter name of contact you would like to modify: ");
+        String modifyingContact = scanner.nextLine();
+        System.out.println("Enter new name: ");
+        String newName = scanner.nextLine();
+        System.out.println("Enter new phone number: ");
+        String newPhoneNumber = scanner.nextLine();
+        mobilePhone.modifyContact(modifyingContact, newName, newPhoneNumber);
+        System.out.println("Contact " + modifyingContact + " has been modified.");
+        System.out.println("New name: " + mobilePhone.getNameContact(newName));
+        System.out.println("New phone number: " + mobilePhone.getPhoneNumberContact(newPhoneNumber));
+    }
+
+    public static void removePhoneContact() {
+        System.out.println("Enter name of contact to be removed: ");
+        String removingContact = scanner.nextLine();
+        mobilePhone.removeContact(removingContact);
+        System.out.println("Contact " + removingContact + " has been removed.");
+    }
+
+    public static void searchPhoneCntact() {
+        System.out.println("Enter name of contact: ");
+        String searchedContact = scanner.nextLine();
+        mobilePhone.contactExists(searchedContact);
+    }
+
+
+
 }
--- ArrayListChallenge/src/com/israelmesa/MobilePhone.java
+++ ArrayListChallenge/src/com/israelmesa/MobilePhone.java
@@ -6,6 +6,10 @@ public class MobilePhone {
 
     private ArrayList<Contacts> phoneList = new ArrayList<Contacts>();
 
+    public ArrayList<Contacts> getPhoneList() {
+        return phoneList;
+    }
+
     public int findContact(String contactName) {
 
         int contactPosition = -1;
@@ -27,6 +31,44 @@ public class MobilePhone {
     public void storeContact(String name, String phoneNumber) {
         Contacts contact = new Contacts(name, phoneNumber);
         phoneList.add(contact);
+    }
+
+    public void modifyContact(String contactName, String newName, String newNumber) {
+        int position = findContact(contactName);
+        Contacts contact = new Contacts(newName, newNumber);
+        phoneList.set(position, contact);
+    }
+
+    public void removeContact(String contactName) {
+        int position = findContact(contactName);
+        phoneList.remove(position);
+    }
+
+    public String getNameContact(String name) {
+        return getPhoneList().get(findContact(name)).getName();
+    }
+
+    public String getPhoneNumberContact(String phoneNumber) {
+        return getPhoneList().get(findContact(phoneNumber)).getPhoneNumber();
+    }
+
+    public Boolean contactExists(String name) {
+        Boolean exists;
+        if (findContact(name) >= 0) {
+            exists = true;
+            System.out.println("Contact " + getNameContact(name) + " exists.");
+        } else {
+            exists = false;
+            System.out.println("Contact " + name + " does not exist");
+        }
+        return exists;
+    }
+
+    public void printMobileContactList() {
+        System.out.println("You have " + getPhoneList().size() + " contacts in your Mobile Phone");
+        for(int i=0; i< getPhoneList().size(); i++) {
+            System.out.println((i+1) + ". " + getPhoneList().get(i));
+        }
     }
 
 
