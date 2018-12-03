9dbec1417f4e483b8593421bb77481ce047e4308
Israel Mesa <mesa.israel50@gmail.com>
2018-12-02 19:47:12 -0500

    findContact - Method

--- ArrayListChallenge/src/com/israelmesa/Main.java
+++ ArrayListChallenge/src/com/israelmesa/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        MobilePhone mobilePhone = new MobilePhone();
+        mobilePhone.storeContact("Juan Valdez", "7221427");
+        mobilePhone.storeContact("Pedro Sanchez", "7221427");
 
 
     }
--- ArrayListChallenge/src/com/israelmesa/MobilePhone.java
+++ ArrayListChallenge/src/com/israelmesa/MobilePhone.java
@@ -4,14 +4,30 @@ import java.util.ArrayList;
 
 public class MobilePhone {
 
-    private ArrayList<Contacts> phoneList;
+    private ArrayList<Contacts> phoneList = new ArrayList<Contacts>();
 
-    public MobilePhone(ArrayList<Contacts> phoneList) {
-        this.phoneList = phoneList;
+    public int findContact(String contactName) {
+
+        int contactPosition = -1;
+
+        for (int i = 0; i < phoneList.size(); i++) {
+            if (phoneList.get(i).getName().compareTo(contactName) == 0) {
+                contactPosition = i;
+            }
+        }
+
+        if (contactPosition < 0) {
+            System.out.println("Name not found");
+        }
+
+        return contactPosition;
+
     }
 
     public void storeContact(String name, String phoneNumber) {
         Contacts contact = new Contacts(name, phoneNumber);
         phoneList.add(contact);
     }
+
+
 }
