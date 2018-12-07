78c981055655575aed3e7df9595f619cb7a6c715
Israel Mesa <mesa.israel50@gmail.com>
2018-12-07 15:26:02 -0500

    Classes finished

--- AutoboxingUnboxingChallengeV2/src/com/israelmesa/Customer.java
+++ AutoboxingUnboxingChallengeV2/src/com/israelmesa/Customer.java
@@ -1,4 +1,27 @@
 package com.israelmesa;
 
+import java.util.ArrayList;
+
 public class Customer {
+
+    private String Name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name, double amount) {
+        Name = name;
+        this.transactions = new ArrayList<Double>();
+        addTransaction(amount);
+    }
+
+    public void addTransaction(double amount) {
+        this.transactions.add(amount);
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
 }
